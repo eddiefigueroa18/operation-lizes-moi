e280faa12f9d68ecf37ed14112444777ffeb4aef
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-10-19 11:21:39 -0700

    generateMarkdown is finally completed and good to go. Need to work on readme of project itself, and correct syntax of entire project to look orderly and neat

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,9 +1,5 @@
-// const { default: inquirer } = require("inquirer");
-
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-
-//create a function that reutrns a license badge based on which license is passed in 
-//if thre is not license , return an empty string
+//create a function that returns a license badge based on which license is passed in 
+//if there is not license , return an empty string
 function renderLicenseBadge(license) {
 if(license === 'Other') {
     return ''
@@ -12,7 +8,25 @@ if(license === 'Other') {
 };
 };
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//create a function that returns a license link based on which license is passed in 
+//if there is not license , return an empty string
+function renderLicenseLink(license) {
+if(license === 'Other') {
+    return ''
+} else if (license === 'Apache') {
+    return ` https://apache.org/licenses/` 
+}  else if (license === 'MIT') {
+    return ` https://mit-license.org/`
+} else if (license === 'Eclipse') {
+    return ` https://www.eclipse.org/legal/epl-2.0/`
+} else if (license === 'Mozilla') {
+    return ` https://www.mozilla.org/en-US/MPL/`
+};
+};
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
 //create a function that returns the license section of README 
 //if there is no license, return an empty string
@@ -24,20 +38,21 @@ function renderLicenseSection(license) {
     }
 };
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
 //create a function to generate markdown for README
 function generateMarkdown(data) {
     return `
 # ${data.title}
-${renderLicenseBadge(data.license)} 
+${renderLicenseBadge(data.license)} This project is: ${data.license} licensed.
+For more information regarding License, Refer to: ${renderLicenseLink(data.license)}
 ## Description
 ${data.description}
 ## Table of Contents 
 * [Description](#Description)
 * [Installation](#Installation)
 * [Usage](#Usage)
-* [Contributions](#Contributing)
+* [Contributing](#Contributing)
 * [Test](#Tests)
 * [License](#License)
 * [Questions](#Questions)
@@ -50,13 +65,15 @@ ${data.contributionGuidelines}
 ## Tests
 ${data.tests}
 ## License 
-${renderLicenseSection(data.license)} This project is ${data.license} licensed.
+${renderLicenseSection(data.license)} This project is: ${data.license} licensed.
 ## Questions
+For more information you can reach me through:
 * Github: https://github.com/${data.github}
-* Email: ${data.email}`;
+* Email: ${data.email}
+    - For email, please give me your name and a brief description of your reason for contacting me `;
 }
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
 module.exports = generateMarkdown;
 
--- generated/README.md
+++ generated/README.md
@@ -1,13 +1,14 @@
 
-# The Greatest README Generator 
-![Mozilla](https://img.shields.io/badge/License-Mozilla-yellowg) 
+# The Greatest README Generator Ever!
+![Eclipse](https://img.shields.io/badge/License-Eclipse-yellowg) This project is: Eclipse licensed.
+For more information regarding License, Refer to:  https://www.eclipse.org/legal/epl-2.0/
 ## Description
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 ## Table of Contents 
 * [Description](#Description)
 * [Installation](#Installation)
 * [Usage](#Usage)
-* [Contributions](#Contributing)
+* [Contributing](#Contributing)
 * [Test](#Tests)
 * [License](#License)
 * [Questions](#Questions)
@@ -20,7 +21,9 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 ## Tests
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 ## License 
-![Mozilla](https://img.shields.io/badge/License-Mozilla-yellowg) This project is Mozilla licensed.
+![Eclipse](https://img.shields.io/badge/License-Eclipse-yellowg) This project is: Eclipse licensed.
 ## Questions
-* Github: https://github.com/asdf
-* Email: asdf
+For more information you can reach me through:
+* Github: https://github.com/eddiefigueroa18
+* Email: eddiefigueroa18@gmail.com
+    - For email, please give me your name and a brief description of your reason for contacting me 
